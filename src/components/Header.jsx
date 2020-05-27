ce95f2b9aceb4a5c0b64b7cc1f90da13a1da2478
Akshay Kannan <akshaykannan9677@gmail.com>
2020-05-27 16:16:54 +0530

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <i class="fas fa-list-alt" /> Post It
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
